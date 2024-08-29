dc9373e9a1cee367aad22bead03771f5dc8791cf
Angela Herrera <hagelate2002@gmail.com>
2024-08-29 10:57:59 -0500

    Curso angular, carpeta 2, video 5, version 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { HomeChildOneComponent } from './home-child-one/home-child-one.component';
-import { HomeChildTwoComponent } from './home-child-two/home-child-two.component';
+// import { HomeComponent } from './home/home.component';
+// import { HomeChildOneComponent } from './home-child-one/home-child-one.component';
+// import { HomeChildTwoComponent } from './home-child-two/home-child-two.component';
 // import { TestComponentComponent } from './test-component/test-component.component';
 // import { Page404Component } from './page404/page404.component';
 
 const routes: Routes = [ 
   {
-    path:'',
-    component: HomeComponent,
-    children:[
-      {
-        path:'one',
-        component:HomeChildOneComponent
-      },
-      {
-        path:'two',
-        component:HomeChildTwoComponent
-      }
-    ]
+    path:'auth', //TODO: http://localhost:4200/auth
+    loadChildren: () => import('@modules/auth/auth.module').then((m) => m.AuthModule)
+  },
+  {
+    path:'task', //TODO: http://localhost:4200/task
+    loadChildren: () => import('@modules/task/task.module').then((m) => m.TaskModule)
   }
+
+
+
+
+
+
+  // {
+  //   path:'',
+  //   component: HomeComponent,
+  //   children:[
+  //     {
+  //       path:'one',
+  //       component:HomeChildOneComponent
+  //     },
+  //     {
+  //       path:'two',
+  //       component:HomeChildTwoComponent
+  //     }
+  //   ]
+  // }
+
+
+
+
   // {
   //   path:'', //TODO http//localhost:4200/
   //   component: TestComponentComponent
--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [ //TODO http://localhost/auth/
+  {
+    path:'',
+    component:LoginPageComponent
+  }
+]; 
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
