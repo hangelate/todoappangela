6a967477d4d60de569b81d38c1063d98abec22aa
Angela Herrera <hagelate2002@gmail.com>
2024-09-01 19:22:50 -0500

    Curso angular, carpeta 4, video 2

--- src/app/modules/task/page/task-page.component.ts
+++ src/app/modules/task/page/task-page.component.ts
@@ -7,15 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskPageComponent implements OnInit {
 
-  showText = true;
-
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  changeText():void{
-    this.showText = !this.showText;
   }
 
 }
